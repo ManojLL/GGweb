9e114f3739fdfd1b927de0143ababec2c7fd9c29
Ashan Perera <ashantiwankaperera@gmail.com>
2022-05-02 18:54:25 +0530

    animations added

--- src/screens/HomePage/index.js
+++ src/screens/HomePage/index.js
@@ -9,8 +9,10 @@ import BulmaTimeline from "./components/Timeline/BulmaTimeline";
 import TeamCard from "./components/TeamCard/TeamCard";
 import RaritySection from "./components/Rarity/RaritySection";
 import FAQ from "./components/FAQ/FAQ";
-import giraffe from '../../images/giraffe.png';
+import giraffe from '../../images/giraffe_throne.png';
 import { Container } from "@material-ui/core";
+import 'animate.css';
+import Fade from 'react-reveal/Fade';
 
 function HomePage() {
   return (
@@ -20,39 +22,45 @@ function HomePage() {
         <div className="hero-head">
           <NavBar/>
         </div>
-        <div className="hero-body">
+        <div className="hero-body landing">
           <section className="container">
             <div className="columns is-vcentered">
               <div className="colum is-half p-6">
                 <div className="container py-6">
-                  <p className="main-top">WELCOME TO THE GOOFY GIRAFFES!</p>
-                  <p className="main-one">Giraffes in the</p>
-                  <p className="main-two">Seven Kingdoms</p>
-                  <p className="main-para">Goofy Giraffes are a set of animals that mutated to have human-like 
-                    traits while still maintaining the appearance of a Giraffe. They take 
-                    traits of characters of famous TV shows while infiltrating their universe to spread their kind.
-                  </p>
-                  <div className="buttons mt-6">
-                    <a className="button is-warning has-text-weight-bold p-5">
-                      <span class="icon">
-                        <i class="fab fa-discord"></i>
-                      </span>
-                      <span>Discord</span>
-                    </a>
-                    <a className="button is-white is-outlined ml-4 has-text-weight-bold p-5">
-                      <span class="icon">
-                        <i class="fab fa-twitter"></i>
-                      </span>
-                      <span>Twitter</span>
-                    </a>
-                  </div>
+                  <Fade top>
+                    <p className="main-top">WELCOME TO THE GOOFY GIRAFFES!</p>
+                    <p className="main-one">Giraffes in the</p>
+                    <p className="main-two">Seven Kingdoms</p>
+                  </Fade>
+                  <Fade bottom>
+                    <p className="">Goofy Giraffes are a set of animals that mutated to have human-like 
+                      traits while still maintaining the appearance of a Giraffe. They take 
+                      traits of characters of famous TV shows while infiltrating their universe to spread their kind.
+                    </p>
+                    <div className="buttons mt-6">
+                      <a className="button is-warning has-text-weight-bold p-5">
+                        <span class="icon">
+                          <i class="fab fa-discord"></i>
+                        </span>
+                        <span>Discord</span>
+                      </a>
+                      <a className="button is-white is-outlined ml-4 has-text-weight-bold p-5">
+                        <span class="icon">
+                          <i class="fab fa-twitter"></i>
+                        </span>
+                        <span>Twitter</span>
+                      </a>
+                    </div>
+                  </Fade>
                 </div>
               </div>
-              <div className="column is-half">
-                <figure class="image">
-                  <img src={giraffe}/>
-                </figure>
-              </div>
+              <Fade right>
+                <div className="column is-half">
+                    <figure class="image animate__animated animate__pulse animate__slower animate__infinite">
+                      <img src={giraffe}/>
+                    </figure>
+                </div>
+              </Fade>
             </div>
           </section>
         </div>
@@ -64,17 +72,19 @@ function HomePage() {
           <div className="container">
             <div className="columns is-vcentered">
               <div className="column is-half p-6">
-                <p className="subtitle">ABOUT OUR PROJECT</p>
-                <p className="title">Goofieverse</p>
-                <p>Goofy Giraffes are a set of animals that mutated to have human-like traits while 
-                  still maintaining the appearance of a Giraffe. They take traits of characters of 
-                  famous TV shows while infiltrating their universe to spread their kind.
-                </p>
-                <br/>
-                <p>It's everyone's fantasy to be a character of their favourite TV show. Heck, 
-                  we act like we are part of it! With our goofy avatars, you'll feel like you actually 
-                  are a part of it! Stay tuned and alert by joining our discord and following us on our socials! Exciting stuff awaits.
-                </p>
+                <Fade bottom>
+                  <p className="subtitle">ABOUT OUR PROJECT</p>
+                  <p className="title">Goofieverse</p>
+                  <p>Goofy Giraffes are a set of animals that mutated to have human-like traits while 
+                    still maintaining the appearance of a Giraffe. They take traits of characters of 
+                    famous TV shows while infiltrating their universe to spread their kind.
+                  </p>
+                  <br/>
+                  <p>It's everyone's fantasy to be a character of their favourite TV show. Heck, 
+                    we act like we are part of it! With our goofy avatars, you'll feel like you actually 
+                    are a part of it! Stay tuned and alert by joining our discord and following us on our socials! Exciting stuff awaits.
+                  </p>
+                </Fade>
               </div>
               <div className="column is-half p-6">
                 <div class="card">
@@ -138,30 +148,42 @@ function HomePage() {
             <p className="title">Meet Team</p>
             <div className="container">
               <div className="columns">
-                <div className="column is-one-third">
-                  <div className="container is-centered">
-                    <TeamCard/>
+                <Fade bottom cascade>
+                  <div className="column is-one-third">
+                    <div className="container is-centered">
+                      <TeamCard/>
+                    </div>
                   </div>
-                </div>
-                <div className="column is-one-third">
-                  <TeamCard/>
-                </div>
-                <div className="column is-one-third">
-                  <TeamCard/>
-                </div>
+                  <div className="column is-one-third">
+                    <div className="container is-centered">
+                      <TeamCard/>
+                    </div>
+                  </div>
+                  <div className="column is-one-third">
+                    <div className="container is-centered">
+                      <TeamCard/>
+                    </div>
+                  </div>
+                </Fade>
               </div>
               <div className="columns">
-                <div className="column is-one-third">
-                  <div className="container is-centered">
-                    <TeamCard/>
+                <Fade bottom cascade>
+                  <div className="column is-one-third">
+                    <div className="container is-centered">
+                      <TeamCard/>
+                    </div>
                   </div>
-                </div>
-                <div className="column is-one-third">
-                  <TeamCard/>
-                </div>
-                <div className="column is-one-third">
-                  <TeamCard/>
-                </div>
+                  <div className="column is-one-third">
+                    <div className="container is-centered">
+                      <TeamCard/>
+                    </div>
+                  </div>
+                  <div className="column is-one-third">
+                    <div className="container is-centered">
+                      <TeamCard/>
+                    </div>
+                  </div>
+                </Fade>
               </div>
             </div>
           </div>
