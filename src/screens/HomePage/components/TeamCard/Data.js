2b8197a7ef6284229e7bf8d18ca7b081ec5096e4
Ashan Perera <ashantiwankaperera@gmail.com>
2022-05-07 00:04:22 +0530

    initital commit

---
+++ src/screens/HomePage/components/TeamCard/Data.js
@@ -0,0 +1,58 @@
+import OGY from '../../../../images/ogy.png'
+
+const data=[
+    {
+        image:{OGY},
+        title:'FOUNDER',
+        name:'NMZ',
+        intro:'The Goofiest of all Giraffes. It is he who founded the Goofy Giraffes project.',
+        descord:'',
+        twitter:'',
+        linkedin:''
+    },
+    {
+        image:{OGY},
+        title:'THE MARKETING WIZARD',
+        name:'URA',
+        intro:'The Goofiest of all Giraffes. It is he who founded the Goofy Giraffes project.',
+        descord:'',
+        twitter:'',
+        linkedin:''
+    },
+    {
+        image:{OGY},
+        title:'THE ARTIST',
+        name:'ZAN',
+        intro:'The Goofiest of all Giraffes. It is he who founded the Goofy Giraffes project.',
+        descord:'',
+        twitter:'',
+        linkedin:''
+    },
+    {
+        image:{OGY},
+        title:'THE DEVELOPER',
+        name:'DEA',
+        intro:'The Goofiest of all Giraffes. It is he who founded the Goofy Giraffes project.',
+        descord:'',
+        twitter:'',
+        linkedin:''
+    },
+    {
+        image:{OGY},
+        title:'COMMUNITY LEAD',
+        name:'NGR',
+        intro:'The Goofiest of all Giraffes. It is he who founded the Goofy Giraffes project.',
+        descord:'',
+        twitter:'',
+        linkedin:''
+    },
+    {
+        image:{OGY},
+        title:'SOCIAL MEDIA MANAGER',
+        name:'ASH',
+        intro:'The Goofiest of all Giraffes. It is he who founded the Goofy Giraffes project.',
+        descord:'',
+        twitter:'',
+        linkedin:''
+    },
+]
