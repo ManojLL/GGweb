30947d4cc631d5ad48813c79ef0553bd9378bb9e
Ashan Perera <ashantiwankaperera@gmail.com>
2022-05-06 22:10:03 +0530

    minor changes 5

--- src/screens/HomePage/components/NavBar/NavBar.js
+++ src/screens/HomePage/components/NavBar/NavBar.js
@@ -4,19 +4,19 @@ import logo from '../../../../images/GoofyGiraffes_logo.png'
 
 function NavBar(){
     return(
-        <nav className="navbar is-black p-4 is-fixed-top">
+        <nav className="navbar is-black p-4 is-fixed-top" role="navigation" aria-label="main navigation">
             <div className="container center-container">
                 <div className="navbar-brand">
                     <a class="navbar-item" href="#">
                         <img src={logo} width="120" height="37"/>
                     </a>
-                    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navContent">
+                    <a role="button" class="navbar-burger" id="nav-toggle" aria-label="menu" aria-expanded="false" data-target="nav-menu">
                         <span aria-hidden="true"></span>
                         <span aria-hidden="true"></span>
                         <span aria-hidden="true"></span>
                     </a>
                 </div>
-                <div className="navbar-menu" id="navContent">
+                <div className="navbar-menu" id="nav-menu">
                     <div className="navbar-end">
                         <a className="navbar-item nav-text">
                             Goofyverse
