ef4841a2a3c3f47a267440c4625fb656220a1be7
Ashan Perera <ashantiwankaperera@gmail.com>
2022-05-07 01:13:49 +0530

    minor changes

--- src/screens/HomePage/components/Rarity/RaritySection.js
+++ src/screens/HomePage/components/Rarity/RaritySection.js
@@ -8,7 +8,7 @@ export default class RaritySection extends Component {
     var settings = {
         infinite: false,
         speed: 500,
-        slidesToShow: 4,
+        slidesToShow: 3,
         slidesToScroll: 1,
         initialSlide: 0,
         autoplay: true,
