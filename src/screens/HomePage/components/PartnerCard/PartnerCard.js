c5b9bea993794dfe10a064bea1597d7678d35e0e
Ashan Perera <ashantiwankaperera@gmail.com>
2022-05-07 20:28:12 +0530

    minor changes

--- src/screens/HomePage/components/PartnerCard/PartnerCard.js
+++ src/screens/HomePage/components/PartnerCard/PartnerCard.js
@@ -5,7 +5,7 @@ import GCF from '../../../../images/GCF-logo.png'
 
 function PartnerCard(){
     return(
-        <div className="p-6">
+        <div className="">
             <div className="columns is-vcentered">
                 <div className="column is-half p-6">
                     <img src={GCF}/>
@@ -13,13 +13,13 @@ function PartnerCard(){
                 <div className="column is-half p-6 has-text-left">
                     <p>United by a common goal, GCF is a family shared commitment to a sustainable 
                         future for all giraffe populations in the wild is governed by a Memorandum of Understanding.</p>
-                        <div className="buttons mt-6">
-                    <a className="button is-warning has-text-weight-bold p-4">
-                      <span>Explore</span>
-                    </a>
-                    <a className="button is-white is-outlined ml-2 has-text-weight-bold p-4">
-                      <span>Our Partnership</span>
-                    </a>
+                    <div className="buttons mt-6">
+                        <a className="button is-warning has-text-weight-bold p-4">
+                        <span>Explore</span>
+                        </a>
+                        <a className="button is-white is-outlined ml-2 has-text-weight-bold p-4">
+                        <span>Our Partnership</span>
+                        </a>
                   </div>
                 </div>
             </div>
