134c1c85b0ffd89f4c9db8af5419d5263439ba80
Ashan Perera <ashantiwankaperera@gmail.com>
2022-05-15 08:39:29 +0530

    minor changes

--- src/screens/HomePage/components/PartnerCard/PartnerCard.js
+++ src/screens/HomePage/components/PartnerCard/PartnerCard.js
@@ -9,20 +9,22 @@ function PartnerCard(){
         <div className="container center-container pt-6">
             <div className="columns is-vcentered">
                 <div className="column is-half">
-                    <div className="columns is-mobile has-text-centered is-vcentered bg-white p-4">
-                        <div className="column px-6">
+                    <div className="columns is-mobile has-text-centered is-vcentered bg-white px-4 py-5">
+                        <div className="column logo-padding">
                             <img src={GCF}/>
                         </div>
-                        <div className="column pr-6">
-                            <img src={VOR} className="px-5"/>
+                        <div className="column logo-padding">
+                            <img src={VOR} className="vor-padding"/>
                         </div>
                     </div> 
                 </div>
-                <div className="column is-half has-text-left paragraph-right">
+                <div className="column is-half has-text-left">
+                    <div className=" paragraph-right">
                     <p className="">If you wish to partner up with us to create a positive impact to the community, feel free to reach out.</p>
-                    <a className="button is-warning has-text-weight-bold mt-6">
+                    <a className="button is-warning has-text-weight-bold mt-4">
                         <span>Contact us</span>
                     </a>
+                    </div>
                 </div>
             </div>
         </div>
